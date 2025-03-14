93314ef21bb1a29ff198a1c8a2f44441fd9c4bbb
Isaiah Muchene <imuchene@msn.com>
2025-03-14 20:35:08 +0300

    Stored PDFs in a vector store

--- openai.ts
+++ openai.ts
@@ -1,78 +1,19 @@
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import { LlamaParseReader, storageContextFromDefaults, VectorStoreIndex } from 'llamaindex';
 
 dotenv.config();
 
+const reader = new LlamaParseReader({ resultType: 'text'});
+const document = await reader.loadData('./embeddings/thesis.pdf');
+const storageContext = await storageContextFromDefaults({
+  persistDir: './storage'
+});
+
+
+console.log('Creating the index');
+const index = await VectorStoreIndex.fromDocuments(document, storageContext);
+
+console.log('index created', index);
+
 const openAI = new OpenAI();
-
-const functions: any = {
-  add: (obj: { num1: number, num2: number}) => {
-    const sum = obj.num1 + obj.num2;
-    console.log('Sum is', sum);
-  },
-  subtract: (obj: { num1: number, num2: number}) => {
-    const difference = obj.num1 - obj.num2;
-    console.log('Difference is', difference);
-  }
-}
-
-
-const response = await openAI.chat.completions.create({
-  model: 'gpt-4o-mini',
-  store: true,
-  messages: [
-    {
-      role: "system",
-      content: "You are a smart calculator."
-    },
-    {
-      role: "user",
-      content: "How much is 10 minus 2?"
-    }
-  ],
-  functions: [
-    {
-      name: 'add',
-      description: 'Adds two numbers',
-      parameters: {
-        type: 'object',
-        properties: {
-          num1: {
-            type: 'number'
-          },
-          num2: {
-            type: 'number'
-          }
-        }
-      }
-    },
-    {
-      name: 'subtract',
-      description: 'Subtracts two numbers',
-      parameters: {
-        type: 'object',
-        properties: {
-          num1: {
-            type: 'number'
-          },
-          num2: {
-            type: 'number'
-          }
-        }
-      }
-    }
-  ]
-})
-
-console.log('response choices', response.choices[0]);
-
-const function_call = response.choices[0].message.function_call
-
-if (function_call) {
-  const name = function_call.name;
-  const args = JSON.parse(function_call.arguments)
-
-  const fn = functions[name];
-
-  fn(args);
-}
