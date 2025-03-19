9cab0eb9afb95341d054d311e2212663a8af4f74
Isaiah Muchene <imuchene@msn.com>
2025-03-19 17:39:56 +0300

    Queried the data store

--- openai.ts
+++ openai.ts
@@ -1,19 +1,48 @@
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
-import { LlamaParseReader, storageContextFromDefaults, VectorStoreIndex } from 'llamaindex';
+import { LlamaParseReader, VectorStoreIndex, Settings, TextNode } from 'llamaindex';
 
 dotenv.config();
 
-const reader = new LlamaParseReader({ resultType: 'text'});
+const reader = new LlamaParseReader({ resultType: 'text' });
 const document = await reader.loadData('./embeddings/thesis.pdf');
-const storageContext = await storageContextFromDefaults({
-  persistDir: './storage'
-});
+
+Settings.chunkSize = 4000;
+Settings.chunkOverlap = 500;
 
 
 console.log('Creating the index');
-const index = await VectorStoreIndex.fromDocuments(document, storageContext);
-
+const index = await VectorStoreIndex.fromDocuments(document);
 console.log('index created', index);
 
+const query = 'Which technologies can be used to solve congestion at airports?';
+
+
+const retriever = index.asRetriever({ similarityTopK: 3 });
+
+const matchingNodes = await retriever.retrieve({ query: query });
+
+const knowledge = matchingNodes.map(node => {
+  const textNode = node.node as TextNode;
+  return textNode.text;
+}).join('\n\n');
+
+// Querying OpenAI
+
 const openAI = new OpenAI();
+const response = await openAI.chat.completions.create({
+  model: 'gpt-4o-mini',
+  temperature: 0,
+  messages: [
+    {
+      role: 'system',
+      content: `You are an aviation expert. Here is your knowledge to answer the user's  question: ${knowledge}`
+    },
+    {
+      role: 'user',
+      content: query,
+    }
+  ]
+})
+
+console.log('response', response.choices[0]);
