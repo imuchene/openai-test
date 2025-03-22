3ada4835a2584b6244ab5e3df21a04688f66b866
Isaiah Muchene <imuchene@msn.com>
2025-03-22 17:22:19 +0300

    Updated the recommender to work with the latest convention of the OpenAI library

--- openai-typescript/recommender-basic.ts
+++ openai-typescript/recommender-basic.ts
@@ -1,6 +1,8 @@
 import '@dotenvx/dotenvx/config';
-import  { OpenAI } from 'openai';
-import { getProductId } from '../realtime-data/lib/get-product-id.js';
+import { OpenAI } from 'openai';
+import { ChatCompletionTool } from 'openai/resources/index';
+import { getProductId } from '../realtime-data/lib/get-product-id';
+
 
 const functions: any = {
   async recommendProduct(obj: { description: string }) {
@@ -17,6 +19,24 @@ const functions: any = {
 
 // Querying OpenAI
 const openAI = new OpenAI();
+
+const tools: ChatCompletionTool[] = [{
+  type: 'function',
+  function: {
+    name: 'recommendProduct',
+    description: 'Takes a description and returns a recommended product',
+    parameters: {
+      type: 'object',
+      properties: {
+        description: {
+          type: 'string',
+          description: "A short description of the product the user is looking for, ideally a copy paste from the user's message"
+        }
+      }
+    }
+  }
+}]
+
 const response = await openAI.chat.completions.create({
   model: 'gpt-4o-mini',
   temperature: 0,
@@ -30,34 +50,21 @@ const response = await openAI.chat.completions.create({
       content: `I'm a skater. I'm looking for skateboarding shoes.`
     }
   ],
-  functions: [
-    {
-      name: 'recommendProduct',
-      description: 'Takes a description and returns a recommended product',
-      parameters: {
-        type: 'object',
-        properties: {
-          description: {
-            type: 'string',
-            description: "A short description of the product the user is looking for, ideally a copy paste from the user's message"
-          }
-        }
-      }
-    }
-  ],
+  tools: tools,
+  store: true,
 })
 
 console.log('response', response.choices[0]);
 
-const function_call = response.choices[0].message.function_call;
+
+const tool_calls = response.choices[0].message.tool_calls;
+
+console.log('tool calls', tool_calls);
 
 
-console.log('function call', function_call);
-
-
-if (function_call) {
-  const fn = functions[function_call.name];
-  const args = JSON.parse(function_call.arguments);
+if (tool_calls) {
+  const fn = functions[tool_calls[0].function.name];
+  const args = JSON.parse(tool_calls[0].function.arguments);
   const result = await fn(args);
   console.log('result', result);
 }
